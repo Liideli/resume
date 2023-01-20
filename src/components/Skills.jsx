9e47d3925ec574d3eb759084a6ab176294081710
Liideli <roope.kasperi.laine@gmail.com>
2023-01-21 01:11:15 +0200

    initial commit

---
+++ src/components/Skills.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Skills extends Component {
+  render() {
+    return (
+      <div>
+        A 23-year-old young man who likes challenges and solving problems to get out of difficult situations. I am applying for my career as versatile as possible work experience and new challenges. I am currently studying ICT in Metropolia University of Applied Sciences and my major is Mobile Solutions. I like to do sports and take care of my own fitness and health. Hobbies include regular gym and cycling.
+      </div>
+    );
+  }
+}
+
+export default Skills;
