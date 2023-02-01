179869eb86ba116a9a7fe724ef8cfe21e79a63f4
Liideli <roope.kasperi.laine@gmail.com>
2023-02-02 01:27:20 +0200

    href

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,11 +8,11 @@ export const NavBar = () => {
       <Container>
         <Navbar.Brand>Resume</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="https://liideli.github.io//about">About</Nav.Link>
-          <Nav.Link href="https://liideli.github.io//contact">Contact</Nav.Link>
-          <Nav.Link href="https://liideli.github.io//skills">Skills</Nav.Link>
-          <Nav.Link href="https://liideli.github.io//work">Work Experience</Nav.Link>
-          <Nav.Link href="https://liideli.github.io//education">Education</Nav.Link>
+          <Nav.Link href="https://liideli.github.io/about">About</Nav.Link>
+          <Nav.Link href="https://liideli.github.io/contact">Contact</Nav.Link>
+          <Nav.Link href="https://liideli.github.io/skills">Skills</Nav.Link>
+          <Nav.Link href="https://liideli.github.io/work">Work Experience</Nav.Link>
+          <Nav.Link href="https://liideli.github.io/education">Education</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
