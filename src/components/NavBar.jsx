9d24a03f1b811c9aaaf9c8b729ebfa1f0dc6cd86
Liideli <roope.kasperi.laine@gmail.com>
2023-02-01 23:55:21 +0200

    fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import About from "./About";
 
 export const NavBar = () => {
   return (
